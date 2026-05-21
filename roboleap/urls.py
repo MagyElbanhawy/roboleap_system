e6416ef934767885995bdba3d5d63e4a04a975b7
MagyElbanhawy <mbanhawy2004@gmail.com>
2026-05-21 21:58:20 +0300

    fix: urls and homepage

--- roboleap/urls.py
+++ roboleap/urls.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 from academy.views import CurrentUserView, LoginView
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Roboleap API is running 🚀")
 
 urlpatterns = [
-    path("admin/",       admin.site.urls),
+    path('', home),  # ✅ homepage works now
+
+    path("admin/", admin.site.urls),
+
     path("api/auth/login/", LoginView.as_view(), name="api-token-auth"),
     path("api/auth/me/", CurrentUserView.as_view(), name="api-current-user"),
-    path("",             include("academy.urls")),
+
+    path("api/", include("academy.urls")),  # 🔥 better practice
 ]
